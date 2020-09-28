0d5c87b05f15f7f6f85d8a05bde8593e4b03b66a
Michele Cattadori <michelecattadori91@gmail.com>
2020-09-28 21:53:58 +0200

    Initial commit
    Start

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //Player's Movement Speed
+    [SerializeField]
+    private float speed;
+
+    protected Animator myAnimator;
+
+    //Player's Direction 
+    protected Vector2 direction;
+
+    private Rigidbody2D myRigidBody;
+
+    protected bool isAttacking = false;
+
+    protected Coroutine attackRoutine;
+
+    public bool IsMoving
+    {
+        get
+        {
+            //Ciao
+            return direction.x != 0 || direction.y != 0;
+        }
+    }
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        //animator = GameObject.Find("Animator").GetComponent<Animator>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        HandleLayers();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void HandleLayers()
+    {
+        if (IsMoving)
+        {
+            ActivateLayer("WalkLayer");
+
+            myAnimator.SetFloat("x", direction.x);
+            myAnimator.SetFloat("y", direction.y);
+
+            StopAttack();
+        }
+        else if (isAttacking)
+        {
+            ActivateLayer("AttackLayer");
+        }
+        else
+        {
+            ActivateLayer("IdleLayer");
+        }
+    }
+
+    //Moves the Player
+    public void Move()
+    {
+        //Make sure that the player moves
+        myRigidBody.velocity = direction.normalized * speed;
+
+        //x = 2, Y = 2 /// Normalize = 1, 1
+
+    }
+
+    public void ActivateLayer(string layerName)
+    {
+        for (int i = 0; i < myAnimator.layerCount; i ++)
+        {
+            myAnimator.SetLayerWeight(i, 0);
+        }
+
+        myAnimator.SetLayerWeight(myAnimator.GetLayerIndex(layerName), 1);
+    }
+
+    public void StopAttack()
+    {
+        if( attackRoutine != null)
+        {
+            StopCoroutine(attackRoutine);
+            Debug.Log("Attack Stopped");
+            isAttacking = false;
+            myAnimator.SetBool("attack", isAttacking);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Player : Character
+{
+
+    [SerializeField]
+    private Stat health;
+
+    [SerializeField]
+    private Stat energy;
+
+    [SerializeField]
+    private float healthValue;
+
+    [SerializeField]
+    private float initHealth;
+
+    private float initEnergy = 100;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        health.Initialize(healthValue, initHealth);
+        energy.Initialize(initEnergy, initEnergy);
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetInput();
+
+        base.Update();
+    }
+
+    //Listen to the Player's input
+    private void GetInput()
+    {
+
+        direction = Vector2.zero;
+
+        ///THIS IS USED FOR DEBUGGING ONLY
+        ///
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            health.MyCurrentValue -= 10;
+            energy.MyCurrentValue -= 10;
+        }
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            health.MyCurrentValue += 10;
+            energy.MyCurrentValue += 10;
+        }
+
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            attackRoutine = StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            myAnimator.SetBool("attack", isAttacking);
+
+            yield return new WaitForSeconds(2); // This is a hardcoded cast time, for debugging
+
+            Debug.Log("Attack Done");
+            StopAttack();
+        }
+    }
+}
